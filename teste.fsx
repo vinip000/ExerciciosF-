d54d872c357b4090d11e958e50cb2983c4475e13
vinip000 <112911506+vinip000@users.noreply.github.com>
2025-07-17 17:42:17 -0300

    Add files via upload

---
+++ teste.fsx
@@ -0,0 +1,12 @@
+// Declarando duas variáveis inteiras
+let a = 10
+let b = 5
+
+// Soma das variáveis
+let soma = a + b
+
+// Verificando se é divisível por 2
+if soma % 2 = 0 then
+    printfn "A soma (%d) é divisível por 2." soma
+else
+    printfn "A soma (%d) NÃO é divisível por 2." soma
